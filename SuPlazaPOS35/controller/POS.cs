38613c2d5ef5898672c45a06fda1bac50aaa097e
Jony322 <jonathan_.torres@hotmail.com>
2023-07-22 14:43:42 -0600

    formatos de fecha hay que hacer la corrección

--- SuPlazaPOS35/controller/POS.cs
+++ SuPlazaPOS35/controller/POS.cs
@@ -578,7 +578,7 @@ namespace SuPlazaPOS35.controller
                     venta_cancelada.venta_cancelada_articulo.Add(venta_cancelada_articulo);
                 }
             }
-            new SuspencionDAO().setSaleSuspended(venta_cancelada);
+            new SuspencionDAO().c(venta_cancelada);
             SaleRecovery = false;
             venta_cancelada = null;
             itemsSales.Clear();
